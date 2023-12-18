2836a3e413d9c2932824d2fa0aaddca7f6591f3a
Kamilla Osmonova <kameakadze@gmail.com>
2023-12-18 17:30:42 +0600

    add lambda expressions to perform actions

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,24 +1,20 @@
-//package task2;
-//
-//import java.util.ArrayList;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//        // исправьте код так, что бы на экран выводилось что-то вроде.
-//        // Я Misty. Я прыгаю!
-//        // Я Tibbles. Я сплю!
-//        // Я Ginger. Я кушаю!
-//        // для решения примените лямбда-выражения,
-//
-//        // каждый кот должен уметь выполнять что-то своё
-//        var cats = new ArrayList<ActiveCat>();
-//        cats.add(new ActiveCat(???));
-//        cats.add(new ActiveCat(???));
-//        cats.add(new ActiveCat(???));
-//        // добавьте ещё два-три кота, с совершенно другими действиями
-//
-//        cats.forEach(ActiveCat::doAction);
-//    }
-//
-//}
+package task2;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var cats = new ArrayList<ActiveCat>();
+        cats.add(new ActiveCat(() -> ActiveCat.jump()));
+        cats.add(new ActiveCat(() -> ActiveCat.sleep()));
+        cats.add(new ActiveCat(() -> ActiveCat.eat()));
+
+        // добавьте ещё два-три кота, с совершенно другими действиями
+        cats.add(new ActiveCat(() -> ActiveCat.play()));
+        cats.add(new ActiveCat(() -> ActiveCat.run()));
+        cats.add(new ActiveCat(() -> ActiveCat.relax()));
+
+        cats.forEach(ActiveCat::doAction);
+    }
+}
