dfe017a28b93b40c97bb1934ee168cceaf962914
Kamilla Osmonova <kameakadze@gmail.com>
2023-12-18 15:28:28 +0600

    Sort the list by cat breed

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -7,10 +7,8 @@ public class Main {
         var cats = Cat.makeCats(10);
         Printer.print(cats);
 
-        // А сюда добавьте код, который будет сортировать коллекцию котов
-        // используйте лямбда-выражения и ссылки на методы
-        // cats.sort(?);
-        // Printer.print(cats);
+        cats.sort((cat1, cat2) -> cat1.getBreed().compareTo(cat2.getBreed()));
+        Printer.print(cats);
     }
 
 }
