1c76fff41f7770125135bba3a613cefebe47007f
Kamilla Osmonova <kameakadze@gmail.com>
2023-12-18 15:30:27 +0600

    sorted the list by name and age.

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -9,6 +9,16 @@ public class Main {
 
         cats.sort((cat1, cat2) -> cat1.getBreed().compareTo(cat2.getBreed()));
         Printer.print(cats);
+
+        cats.sort((cat1, cat2) -> {
+            if (cat1.getName().equals(cat2.getName())) {
+                return Integer.compare(cat1.getAge(), cat2.getAge());
+            } else {
+                return cat1.getName().compareTo(cat2.getName());
+            }
+        });
+        Printer.print(cats);
+
     }
 
 }
